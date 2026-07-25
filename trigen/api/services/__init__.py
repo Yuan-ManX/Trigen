396e1d50adde8bb2e41c87d4f0bf4a26146f5551
Yuan-Man <ym1076302261@163.com>
2026-07-25 19:28:39 +0800

    fix: update Trigen API Service Layer

--- trigen/api/services/__init__.py
+++ trigen/api/services/__init__.py
@@ -1,6 +1,6 @@
-"""Trigen API 服务层 / Trigen API Service Layer."""
+"""Trigen API Service Layer."""
 
 from trigen.api.services.agent_service import AgentService
 from trigen.api.services.session_service import SessionService
 
-__all__ = ["AgentService", "SessionService"]  # 对外导出的服务类 / Exported service classes
+__all__ = ["AgentService", "SessionService"]  # Exported service classes
